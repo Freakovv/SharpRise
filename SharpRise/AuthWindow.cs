0d24b6545dc8ec624f0afdf9ad681ca1446a866b
Freakovv <misha.simanovich@mail.ru>
2025-03-19 00:13:01 +0300

    login window layout

---
+++ SharpRise/AuthWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpRise
+{
+    internal partial class AuthWindow : Form
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+    }
+}
---
+++ SharpRise/Program.cs
@@ -0,0 +1,13 @@
+namespace SharpRise
+{
+    internal static class Program
+    {
+        // Entry point
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new AuthWindow());
+        }
+    }
+}
