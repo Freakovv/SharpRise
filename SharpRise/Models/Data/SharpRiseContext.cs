2067b6f1226eb103d6283ece3d470aedc6ac5590
Freakovv <misha.simanovich@mail.ru>
2025-03-23 22:41:46 +0300

    EntityFramework implementation

---
+++ SharpRise/Models/Data/SharpRiseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharpRise.Models.Users;
+
+namespace SharpRise.Models.Data
+{
+    public class SharpRiseContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public SharpRiseContext(DbContextOptions<SharpRiseContext> options)
+            : base(options)
+        {
+        }
+    }
+}
+
