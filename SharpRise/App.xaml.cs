ff779410a7671b65204c402e7ad8f28dc65b0234
Freakovv <misha.simanovich@mail.ru>
2025-03-23 22:45:10 +0300

    Loading the configurator~

--- SharpRise/App.xaml.cs
+++ SharpRise/App.xaml.cs
@@ -2,22 +2,35 @@
 using System.Threading.Tasks;
 using System.Windows;
 using SharpRise;
+using SharpRise.ViewModels;
 using WinFormsApp = System.Windows.Forms.Application;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace SharpRiseIntegration
 {
     public partial class App : System.Windows.Application
     {
+        public IConfiguration Configuration { get; private set; }
+
         protected override void OnStartup(StartupEventArgs e)
         {
+            // Загрузка конфигурации из appsettings.json
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
             base.OnStartup(e);
 
-            ViewModelsWPF.SplashScreen splashScreen = new ViewModelsWPF.SplashScreen();
+            SharpRise.ViewModelsWPF.SplashScreen splashScreen = new SharpRise.ViewModelsWPF.SplashScreen();
             splashScreen.Show();
 
+            int splashScreenDelay = Configuration.GetValue<int>("AppSettings:SplashScreenDelay");
             Task.Run(() =>
             {
-                Thread.Sleep(3000); 
+                Thread.Sleep(splashScreenDelay);
 
                 this.Dispatcher.Invoke(() =>
                 {
