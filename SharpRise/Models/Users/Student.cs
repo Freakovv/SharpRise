51511cc5bfebf413e1fe6faf522e0431dc572739
Freakovv <misha.simanovich@mail.ru>
2025-03-23 22:33:35 +0300

    adding entities

---
+++ SharpRise/Models/Users/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SharpRise.Models.Users
+{
+    public class Student
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [MaxLength(30)] // SharpRise стандарт
+        public required string Username { get; set; }
+
+        [MaxLength(64)] // SHA-256 стандарт
+        public required string Password { get; set; }
+
+        [MaxLength(254)] // RFC 5321 стандарт
+        public required string Email { get; set; }
+
+        // Навигационное свойство для связи "многие ко многим"
+        public ICollection<Group> Groups { get; set; } = new List<Group>();
+    }
+}
---
+++ SharpRise/Models/Users/Teacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpRise.Models.Users
+{
+    public class Teacher
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(30)] // SharpRise стандарт
+        public required string Username { get; set; }
+
+        [MaxLength(64)] // SHA-256 стандарт
+        public required string Password { get; set; }
+
+        [MaxLength(254)] // RFC 5321 стандарт
+        public required string Email { get; set; }
+
+        // Навигационное свойство для связи "один ко многим" с группами
+        public ICollection<Group> Groups { get; set; } = new List<Group>();
+    }
+}
