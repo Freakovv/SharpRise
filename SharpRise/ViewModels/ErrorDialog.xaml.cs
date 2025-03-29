e34ab394a100096329e97d5653d308625cb6014a
Freakovv <misha.simanovich@mail.ru>
2025-03-29 14:23:34 +0300

    ErrorDialog View

---
+++ SharpRise/ViewModels/ErrorDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace SharpRise.ViewModels
+{
+        public partial class ErrorDialog : Window
+        {
+            public ErrorDialog()
+            {
+                InitializeComponent();
+            }
+
+            private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+            {
+                try
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = e.Uri.AbsoluteUri,
+                        UseShellExecute = true
+                    });
+                    e.Handled = true;
+                }
+                catch
+                {
+                    System.Windows.MessageBox.Show("Не удалось открыть ссылку", "Ошибка",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+}
