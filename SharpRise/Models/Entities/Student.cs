b913421d2aab7ed7038573b1c3d92884e1cd89ae
Freakovv <misha.simanovich@mail.ru>
2025-04-13 23:00:52 +0300

    Renamed the folder to better match the files

---
+++ SharpRise/Models/Entities/Student.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SharpRise.Models.Users
+{
+    public class Student : User
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [MaxLength(30)] // SharpRise стандарт
+        public required string Username { get; set; }
+
+        [MaxLength(64)] // SHA-256 стандарт
+        public required string Password { get; set; }
+
+        [MaxLength(254)] // RFC 5321 стандарт
+        public required string Email { get; set; }
+
+        // Навигационное свойство для связи "многие ко многим"
+        public ICollection<Group> Groups { get; set; } = new List<Group>();
+
+        public Student Parse(Teacher teacher)
+        {
+            Id = teacher.Id;
+            Username = teacher.Username;
+            Password = teacher.Password;
+            Email = teacher.Email;
+            Groups = teacher.Groups;
+            return this;
+        }
+    }
+}
